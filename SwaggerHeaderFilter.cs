9e971a444c2167794aa7b8a6c7e7a432d89929b6
عبدالرحمن مصطفي حسين محمد <AbdAlrahaman.MOUSTAFA700@el-eng.menofia.edu.eg>
2026-04-26 15:18:47 +0300

    Edit Swagger header filter to document X-User-Id in API

--- SwaggerHeaderFilter.cs
+++ SwaggerHeaderFilter.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
-namespace Electric_Power_Monitoring_System  // غيّر الاسم حسب موقعك
+namespace Electric_Power_Monitoring_System
 {
     public class AddRequiredHeaderParameter : IOperationFilter
     {
         public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
+            // الحصول على اسم الـ Controller و Action
+            var controllerName = context.MethodInfo.DeclaringType?.Name;
+            var actionName = context.MethodInfo.Name;
+
+            // قائمة الـ Controllers التي تحتاج إلى X-User-Id
+            var targetControllers = new[] { "HubsController", "NotificationsController", "ConsumptionController", "CompareController" };
+            // أو نضيف استثناء للـ ReadingsController
+            if (controllerName == "ReadingsController")
+                return; // لا نضيف الهيدر لـ ReadingsController
+
             operation.Parameters ??= new List<OpenApiParameter>();
             operation.Parameters.Add(new OpenApiParameter
             {
